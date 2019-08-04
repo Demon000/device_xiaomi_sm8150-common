49e9c34a237b18bda99f681bcb20003b5bf32cbd
Demon000 <demonsingur@gmail.com>
2019-08-04 10:25:39 +0200

    sm8150-common: copy default media configs
    Change-Id: Ieb49e850a0b1009fa238b8b7ef36f09f0550badb

--- sm8150.mk
+++ sm8150.mk
@@ -119,6 +119,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
