ac9c0feb516950adf325365e4f8d495c5baefb6e
Demon000 <demonsingur@gmail.com>
2019-08-09 01:51:33 +0200

    sm8150-common: remove custom power HAL
    Change-Id: I3f860c6db1b4118f59fd9ed2ab7586a9e8454dae

--- sm8150.mk
+++ sm8150.mk
@@ -173,10 +173,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_sm8150
-
 # QTI
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
