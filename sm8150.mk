28051f78486afad4eb3ad2b0b5fdf051e306258f
Demon000 <demonsingur@gmail.com>
2019-08-13 20:39:51 +0200

    sm8150-common: configs: add ueventd configuration file
    Change-Id: If331bc2f66f52dfddaef7628a7a58241279cc325

--- sm8150.mk
+++ sm8150.mk
@@ -198,6 +198,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# Uevent
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
+
 # USB
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
