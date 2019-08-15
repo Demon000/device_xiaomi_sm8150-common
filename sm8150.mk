d62992aa6f1f04a4f0f58b8a0a9e28f576e117f9
Demon000 <demonsingur@gmail.com>
2019-08-15 16:23:59 +0200

    sm8150-common: build renderscript stack
    Change-Id: I63a4a0a220feec5e861e93feb4d45a33facf0e1b

--- sm8150.mk
+++ sm8150.mk
@@ -257,6 +257,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_xiaomi
 
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
