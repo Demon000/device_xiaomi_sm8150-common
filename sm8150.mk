540f94e9a760b6c5f20d7fd5db0fb0c6c1997243
Demon000 <demonsingur@gmail.com>
2019-08-15 12:49:37 +0200

    sm8150-common: build sensors stack
    Change-Id: I21eeb33002adfe8e56c7e14b3f9257028220dc7b

--- sm8150.mk
+++ sm8150.mk
@@ -247,6 +247,10 @@ PRODUCT_PACKAGES += \
     librecovery_updater_xiaomi
 
 # Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
