fd6d8c507b290af029cb237262909d5459f68b41
Demon000 <demonsingur@gmail.com>
2019-08-17 23:18:53 +0200

    sm8150-common: add wifi interface prop for ims

--- common-props.mk
+++ common-props.mk
@@ -193,3 +193,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
+
+# WiFi
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0
