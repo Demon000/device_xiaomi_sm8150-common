c69fd08f9987314e3f3bc8c5fc73b77d5264c488
Demon000 <demonsingur@gmail.com>
2019-10-21 00:44:37 +0200

    sm8150-common: update config.fs entries to match MIUI 9.9.6
    Change-Id: I26d1b42d407d047a46469bbe24a496cb6c38e2b0

--- config.fs
+++ config.fs
@@ -54,7 +54,7 @@ caps: NET_BIND_SERVICE
 
 [vendor/bin/imsrcsd]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE WAKE_ALARM BLOCK_SUSPEND
 
@@ -102,7 +102,7 @@ caps: 0
 
 [vendor/firmware_mnt/image/*]
 mode: 0771
-user: AID_ROOT
+user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
