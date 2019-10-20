3aa7e51c8431369cda755a38bd0b1591d64c1e9d
Demon000 <demonsingur@gmail.com>
2019-10-21 00:00:39 +0200

    sm8150-common: seccomp_policy: remove unused media extractor policy
    Change-Id: I792cc8a705c626b428f7d0566c455f25c633e1ec

--- sm8150.mk
+++ sm8150.mk
@@ -213,8 +213,7 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
