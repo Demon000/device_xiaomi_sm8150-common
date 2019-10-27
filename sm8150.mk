fb6329973edefbeb59bba05ee7fd612ae884d8aa
Demon000 <demonsingur@gmail.com>
2019-10-27 10:28:08 +0100

    sm8150-common: do not build bluetooth audio hal
    Change-Id: I1ac27805175f42968b08fad135a7a18916f94454

--- sm8150.mk
+++ sm8150.mk
@@ -40,12 +40,10 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
-    audio.bluetooth.default \
     audio.usb.default \
     audio.r_submix.default
 
