763a3950be6c7da11de423973988924781e7a172
Demon000 <demonsingur@gmail.com>
2019-11-28 02:34:41 +0100

    sm8150-common: build QTI value add check package
    Change-Id: If5ae3d5d00ddd6a601b238e9968ed4ca11b71db6

--- sm8150.mk
+++ sm8150.mk
@@ -262,6 +262,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 # QTI
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product_privapp-permissions-qti.xml
