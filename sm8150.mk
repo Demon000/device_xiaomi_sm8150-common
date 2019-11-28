493af4ce23bd3e0d9d903ca4739d26de495087c9
Demon000 <demonsingur@gmail.com>
2019-11-28 02:06:54 +0100

    sm8150-common: build RIL packages
    Change-Id: Iad4c8376fd4ca797d554fdee45994879ea1ac0a8

--- sm8150.mk
+++ sm8150.mk
@@ -266,6 +266,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product_privapp-permissions-qti.xml
 
+# RIL
+PRODUCT_PACKAGES += \
+    libjson \
+    libril \
+    librilutils
+
 # RCS
 PRODUCT_PACKAGES += \
     rcs_service_aidl \
