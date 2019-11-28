9e09e227ae57b4745ae259dde2d30f38402c4dd4
Demon000 <demonsingur@gmail.com>
2019-11-28 03:05:16 +0100

    sm8150-common: build WiFi blobs
    Change-Id: I1a59fe37aff8eb6ad413dff77adfa94bf03b5867

--- sm8150.mk
+++ sm8150.mk
@@ -373,11 +373,14 @@ PRODUCT_PACKAGES += \
     wifi-mac-generator
 
 PRODUCT_PACKAGES += \
-   vendor.qti.hardware.wifi.hostapd@1.1 \
-   vendor.qti.hardware.wifi.supplicant@2.1
+    android.hardware.wifi@1.0-service \
+    hostapd \
+    libwifi-hal-qcom \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
