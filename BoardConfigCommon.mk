c04b752052b9daec08885fa0e4f40ee88d810996
Demon000 <demonsingur@gmail.com>
2020-02-10 16:38:48 +0000

    sm8150-common: update security patch level
    From MIUI 11.0.7.0.
    
    Change-Id: I9199a0877f15863fd3a16f2465bc253aa20177cc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -151,7 +151,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2018-08-05
+VENDOR_SECURITY_PATCH := 2019-12-01
 
 # Sepolicy
 include device/qcom/sepolicy/sepolicy.mk
