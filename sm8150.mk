abdfa005ac3cbaf23e8ab12cc6cd0c81498d06fd
Demon000 <demonsingur@gmail.com>
2020-03-27 22:36:39 +0100

    sm8150-common: rootdir: remove unused panel info script
    Change-Id: I5d6e076002829c16b17bce6c0acc7687e71aa43d

--- sm8150.mk
+++ sm8150.mk
@@ -117,9 +117,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/init.panel_info.sh:$(TARGET_COPY_OUT_VENDOR)/etc/init.panel_info.sh
-
-PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.post_boot.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh
 
