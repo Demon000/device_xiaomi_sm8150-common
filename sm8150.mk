5e9264430fd33d89680249956d3ab11d6553f0a1
Demon000 <demonsingur@gmail.com>
2020-05-18 01:48:34 +0200

    sm8150-common: build power service from source
    Change-Id: Iba9c1d1a6d74c318d68aed4fa548d9539b1bc983

--- sm8150.mk
+++ sm8150.mk
@@ -273,6 +273,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.3-service.xiaomi_sm8150
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
